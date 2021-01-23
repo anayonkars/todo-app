88da786337a16f578e8514ec93450f5256e0d76f
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-23 12:58:38 +0530

    Added function

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <ul id="foo">
-    <li>bar1</li>
-    <li>bar2</li>
-    <li>bar3</li>
-  </ul>,
-  document.getElementById("root")
-);
+function MyApp() {
+  return (
+    <ul id="foo">
+      <li>bar1</li>
+      <li>bar2</li>
+      <li>bar3</li>
+    </ul>
+  );
+}
+
+ReactDOM.render(MyApp(), document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
