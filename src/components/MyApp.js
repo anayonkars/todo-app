6022af6b9edbf4456fbdc0fe66d21fd6168146b6
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-26 12:18:23 +0530

    Added MyApp

---
+++ src/components/MyApp.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+function MyApp() {
+  return <h1>Hello from MyApp</h1>;
+}
+
+export default MyApp;
--- src/index.js
+++ src/index.js
@@ -2,29 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import App from "./App";
+import MyApp from "./components/MyApp";
 
-function onButtonClick() {
-  window.open("http://www.google.com");
-}
-
-function MyApp() {
-  return (
-    <div>
-      <button onClick={onButtonClick} className="btn btn-primary btn-sm btn-m2">
-        Go
-      </button>
-      {/* <button
-        href="http://www.google.com"
-        className="btn btn-primary btn-sm btn-m2"
-      >
-        Reset
-      </button> */}
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<MyApp />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
