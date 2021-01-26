259a112d1d7849cfb3770bedcb9762d4cdbc5a96
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-26 12:34:54 +0530

    Added todo items

--- src/components/MyContent.js
+++ src/components/MyContent.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 
 function MyContent() {
+  var todoItems = [
+    "Todo Item",
+    "Some another todo item",
+    "Yet another todo item",
+  ];
   return (
     <div>
       <main>
         <p>Content from MyContent</p>
+        <ul>
+          {todoItems.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
+        </ul>
       </main>
     </div>
   );
