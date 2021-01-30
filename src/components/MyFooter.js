e834992b57e2b95e215c0576f5cb88dbd0133009
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 20:25:53 +0530

    Removed navbar from footer

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -1,6 +1,5 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-const MyFooter = () => <footer className={"navbar"}>Footer from MyFooter</footer>;
-
+const MyFooter = () => <footer>Footer from MyFooter</footer>;
 
 export default MyFooter;
