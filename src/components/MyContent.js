56f0b558c82936d130bc63cc615fcd89b0894848
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 15:52:29 +0530

    Refactoring

--- src/components/MyContent.js
+++ src/components/MyContent.js
@@ -3,29 +3,11 @@ import TodoItem from "./TodoItem";
 
 class MyContent extends Component {
   render() {
-    var handleChange = (event) => {
-      console.log(event);
-    };
     return (
       <div>
         <main>
           <p>Content from MyContent</p>
           <ul>
-            {/*{todoItems.map((todoItem) => (
-            // <li>{todoItem}</li>
-            <div>
-              <label>
-                <input type="checkBox" onChange={handleChange} />
-                {todoItem}
-              </label>
-              <br />
-            </div>
-          ))}*/}
-            {/*<li>
-              <TodoItem item="Todo Item" />
-              <TodoItem item="Some another todo item" />
-              <TodoItem item="yet another todo item" />
-            </li>*/}
             {this.props.todoItems.map((todoItem) => (
               <TodoItem item={todoItem} />
             ))}
