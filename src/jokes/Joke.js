91924db2eb8aa6fbedb846b1b0bbbbe17f8647a5
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 21:15:39 +0530

    Added conditional rendering for question

--- src/jokes/Joke.js
+++ src/jokes/Joke.js
@@ -4,9 +4,7 @@ import PropTypes from "prop-types";
 function Joke(props) {
   return (
     <div>
-      <p style={{ display: props.question ? "block" : "none" }}>
-        Question: {props.question}
-      </p>
+      {props.question ? <p>Question: {props.question}</p> : null}
       <p style={{ display: !props.punchline && "none" }}>
         Punchline: {props.punchline}
       </p>
