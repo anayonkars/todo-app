976eb37b8537e031424ed4257132abde7bbfc036
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 22:22:49 +0530

    Added style for currency

--- src/products/Product.js
+++ src/products/Product.js
@@ -5,7 +5,17 @@ function Product(props) {
     <div>
       {props.id ? <p>Id: {props.id}</p> : <p>There is no id</p>}
       {props.name ? <p>Name: {props.name}</p> : <p>There is no name</p>}
-      {props.price ? <p>Price: {props.price}</p> : <p>There is no price</p>}
+      {props.price ? (
+        <p>
+          Price:
+          {props.price.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}
+        </p>
+      ) : (
+        <p>There is no price</p>
+      )}
       {props.description ? (
         <p>Description: {props.description}</p>
       ) : (
