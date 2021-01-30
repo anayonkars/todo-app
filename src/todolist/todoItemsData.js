a83f2073cca897fc211627cda6791b8ffcd363f9
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 22:27:06 +0530

    Added data file for todo items

---
+++ src/todolist/todoItemsData.js
@@ -0,0 +1,19 @@
+const todoItems = [
+  {
+    description: "Todo item 1",
+  },
+  {
+    description: "Todo item 2",
+  },
+  {
+    description: "Todo item 3",
+  },
+  {
+    description: "Todo item 4",
+  },
+  {
+    description: "Todo item 5",
+  },
+];
+
+export default todoItems;
