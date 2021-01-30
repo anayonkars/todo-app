51cea6aea8908c3edaae15ceb4d36cf3a9aa7b70
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 11:35:04 +0530

    Customized greeting message with time

--- src/components/MyGreeting.js
+++ src/components/MyGreeting.js
@@ -4,9 +4,20 @@ class MyGreeting extends Component {
   render() {
     const { firstName, lastName } = this.props;
     const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours > 12 && hours < 17) {
+      timeOfDay = "afternoon";
+    } else {
+      timeOfDay = "night";
+    }
     return (
       <div>
-        <h1>Hello, {`${firstName} ${lastName}`}</h1>
+        <h1>
+          Good {timeOfDay}, {`${firstName} ${lastName}`}
+        </h1>
         <h3>It is currently about {date.getHours() % 12} o'clock </h3>
       </div>
     );
