4fd78ba243f20a0f82375c6eb8c742ab785dd1ef
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 11:31:18 +0530

    Added time in message

--- src/components/MyGreeting.js
+++ src/components/MyGreeting.js
@@ -1,13 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class MyGreeting extends Component {
-    render() {
-        const {
-            firstName,
-            lastName
-        } = this.props;
-        return <h1>Hello, {`${firstName} ${lastName}`}</h1>
-    }
+  render() {
+    const { firstName, lastName } = this.props;
+    const date = new Date();
+    return (
+      <div>
+        <h1>Hello, {`${firstName} ${lastName}`}</h1>
+        <h3>It is currently about {date.getHours() % 12} o'clock </h3>
+      </div>
+    );
+  }
 }
 
 export default MyGreeting;
