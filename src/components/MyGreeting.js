b2f7b01ebef9f78d78641da92c8bd45629d2e9db
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 15:30:24 +0530

    Added conditional styles

--- src/components/MyGreeting.js
+++ src/components/MyGreeting.js
@@ -7,15 +7,17 @@ class MyGreeting extends Component {
     const hours = date.getHours();
     let timeOfDay;
     const styles = {
-      color: "#FF8C00",
-      backgroundColor: "#FF2D00",
+      fontSize: 30,
     };
     if (hours < 12) {
       timeOfDay = "morning";
+      styles.color = "#04756F";
     } else if (hours > 12 && hours < 17) {
       timeOfDay = "afternoon";
+      styles.color = "#2E0927";
     } else {
       timeOfDay = "night";
+      styles.color = "#D90000";
     }
     return (
       <div>
