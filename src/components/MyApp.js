561d860a1b973d5235cac74426a0d529a6082911
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 11:10:43 +0530

    Added greeting message

--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import MyContent from "./MyContent";
 import MyFooter from "./MyFooter";
 import MyHeader from "./MyHeader";
+import MyGreeting from "./MyGreeting";
 
 function MyApp() {
   return (
     <div>
       <MyHeader />
+      <MyGreeting />
       <MyContent />
       <MyFooter />
     </div>
---
+++ src/components/MyGreeting.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+const MyGreeting = () => <h1>Hello, World!</h1>
+
+export default MyGreeting;
