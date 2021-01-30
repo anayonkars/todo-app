96f8cf4470e48eac9cd2ac680398208d51d69d15
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 20:44:02 +0530

    Removed TodoItem component and passed props via function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,15 +2,13 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "../style.css";
 
-class TodoItem extends Component {
-  render() {
-    return (
-      <div className="todoitem">
-        <input type="checkbox" />
-        {this.props.item}
-      </div>
-    );
-  }
+function TodoItem(props) {
+  return (
+    <div className="todoitem">
+      <input type="checkbox" />
+      {props.item}
+    </div>
+  );
 }
 
 export default TodoItem;
