890e093ce4787f5022878cf6c18a2d79ee2a2d2f
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 10:43:22 +0530

    Modified syntax for function

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
-function MyFooter() {
-  return <footer className={"navbar"}>Footer from MyFooter</footer>;
-}
+const MyFooter = () => <footer className={"navbar"}>Footer from MyFooter</footer>;
+
 
 export default MyFooter;
