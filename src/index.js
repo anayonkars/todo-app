b0f43917103c7abc368c7edd368e04dd13848993
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-01-30 20:47:41 +0530

    Renamed components dir to todolist since those are not components anymore

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import MyApp from "./components/MyApp";
+import MyApp from "./todolist/MyApp";
 
 // function onButtonClick() {
 //   window.open("http://www.google.com");
